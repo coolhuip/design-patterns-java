acbe406085c3e422c14874e6a91a521b30329d0e
Danny Han <cool.huip@gmail.com>
2023-04-20 04:12:36 -0400

    routine

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,8 @@
 /**
  * You have a right to think freely.
  */
-import learn_interface_abstract.Cat;
-import learn_interface_abstract.Monkey;
+//import learn_interface_abstract.Cat;
+//import learn_interface_abstract.Monkey;
 
 import static app.codge.utilities.Utils.print;
 import static app.codge.utilities.Utils.newline;
@@ -12,27 +12,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        print();
-
-        Cat myCat = new Cat("brownie");
-        myCat.printName();
-        myCat.makeNoise();
-        myCat.move();
-
+        print("===================================================");
         newline();
 
-        Monkey cGeorge = new Monkey("curious george");
-        Monkey tGeorge = new Monkey("tired george", true);
-        cGeorge.printName();
-        cGeorge.makeNoise();
-        cGeorge.move();
-        cGeorge.stateOfMind();
-        newline();
-        tGeorge.printName();
-        tGeorge.makeNoise();
-        tGeorge.move();
-        tGeorge.stateOfMind();
+//        Cat myCat = new Cat("brownie");
+//        myCat.printName();
+//        myCat.makeNoise();
+//        myCat.move();
+//
+//        newline();
+//
+//        Monkey cGeorge = new Monkey("curious george");
+//        Monkey tGeorge = new Monkey("tired george", true);
+//        cGeorge.printName();
+//        cGeorge.makeNoise();
+//        cGeorge.move();
+//        cGeorge.stateOfMind();
+//        newline();
+//        tGeorge.printName();
+//        tGeorge.makeNoise();
+//        tGeorge.move();
+//        tGeorge.stateOfMind();
 
+
+
+
+
+
+        newline();
+        print("===================================================");
     }
 
 }
