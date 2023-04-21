5280484f2a18c59314eee2b58e6f323ba87beb85
Danny Han <cool.huip@gmail.com>
2023-04-21 18:43:02 -0400

    added comments (line 2)

--- src/main/reviews/SingletonReview.java
+++ src/main/reviews/SingletonReview.java
@@ -1,15 +1,15 @@
 package reviews;
+// When defining package, EXCLUDE THE MODULE DIRECTORY.
 
 public class SingletonReview {
-
     private static volatile SingletonReview instance;
-    private String data;
+    String data;
 
     private SingletonReview(String data) {
         this.data = data;
     }
 
-    private SingletonReview getInstance(String data) {
+    public static SingletonReview getInstance(String data) {
         SingletonReview result = instance;
         if (result == null) {
             synchronized (SingletonReview.class) {
@@ -21,5 +21,4 @@ public class SingletonReview {
         }
         return result;
     }
-
 }
